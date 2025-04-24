53aab41ba20d56d5050ae386395ae217687989ab
omarboljone <omarboljone@gmail.com>
2025-04-24 13:50:41 +0700

    Create _document.tsx

---
+++ app/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+Good to know:
