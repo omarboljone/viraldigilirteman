b0c214dd3a00b5a5fafe71e521baf8cc0faad820
omarboljone <omarboljone@gmail.com>
2025-04-24 15:26:47 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -157,9 +157,10 @@ export default async function Video({ params }: PageProps) {
 		dangerouslySetInnerHTML={{
          __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1058210})`,
       }}
-   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/><ins id="1058210" data-width="300" data-height="262"></ins><Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2x294z2b4&u=http%3A%2F%2Fwww.juicyads.rocks"/><Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2x294z2b4&u=http%3A%2F%2Fwww.juicyads.rocks"/>
+   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/><ins id="1058210" data-width="300" data-height="262"></ins><Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2x294z2b4&u=http%3A%2F%2Fwww.juicyads.rocks"/><Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2x294z2b4&u=http%3A%2F%2Fwww.juicyads.rocks"/><script
 dangerouslySetInnerHTML={{
-         __html: `var juicy_tags = ['a', 'img']`,
+         __html: `var juicy_tags = ['a', 'img']`, }}
+   />
             <h2 className="text-2xl font-bold text-center my-4">Related Video {file.file_title}
             </h2>
             <SearchCardList query={file.file_title.split(" ")[2]} />
